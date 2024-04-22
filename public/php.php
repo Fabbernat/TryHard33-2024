5febbbc2aa5975e9ab7b0a26975368b2ae4adcd6
Fabbernat <h259147@stud.u-szeged.hu>
2024-04-22 13:41:06 +0200

    php.php unified

--- public/php.php
+++ public/php.php
@@ -197,7 +197,24 @@ include_once "inc/navbar.inc.php"; ?>
                 </pre>
         </div>
     </section>
-
+    <div id="progress_tracker">
+        <br>
+        <form action="python.php" method="post" class="progress-form">
+            <h1>Track your progress!</h1>
+            <!-- Use a span to create a circle -->
+            <label for="trackProgress">
+                <input type="checkbox" id="trackProgress" name="trackProgress" onclick="toggleCircle(this)">
+                <span class="checkmark"></span>
+                I have learned this lesson
+            </label>
+            <button type="submit" name="save_progress" onclick="saveProgress(event)">Save</button>
+            <?php
+            if (isset($_POST["save_progress"])) {
+                $html_completed = true;
+            }
+            ?>
+        </form>
+    </div>
 </main>
 
 <div class="two-px-border white_background" style="border-radius: 20px; width: 30%; margin: 0 auto">
